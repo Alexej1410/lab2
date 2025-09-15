51910f3d61886d386b117a9e99279a9dce507e7d
Alexej1410 <kravchenko.alek5ej@yandex.com>
2025-09-15 15:43:28 +0300

    Add files via upload

---
+++ task3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	puts("Вариант 7\n");
+	int n = 2;
+	int L = 353;
+	float k = n / L;
+	float m = n % L;
+	printf("Дано:\n\t %d\n\t%d\n       -----------\n Ответ:\n\t+%2.2d.%4.4d", n, L, k, m);
+}
